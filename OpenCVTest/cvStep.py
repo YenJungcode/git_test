3e59d3928ac7111f77cc4f582657a33ac988b68f
JackyQLCai <JackyQLCai@mobiledrivetech.com>
2023-11-13 15:14:39 +0800

    Add some function to cvStep

--- OpenCVTest/cvStep.py
+++ OpenCVTest/cvStep.py
@@ -1,32 +1,44 @@
 import time
 import cv2
+import os
+import numpy as np
 
+from skimage.metrics import structural_similarity as ssim
+from skimage.metrics import mean_squared_error as mse
+from skimage import io, color
+from scipy.signal import correlate2d
 
-def setCamera(device):
+def setCamera(device=0):
     global deviceCamera
     # 0 represents the default camera, change if you have multiple cameras
     deviceCamera = device
+    print("Camera set successful !")
 
 
 # Capture a frame
 def captureImage():
     global deviceCamera
-    capture = cv2.VideoCapture(deviceCamera)
+    try:
+        capture = cv2.VideoCapture(deviceCamera)
+    except:
+        assert False, "Unable to set the camera !"
+
     if not capture.isOpened():
-        assert "Cannot open camera !!!"
+        assert False, "Cannot open camera !!!"
 
     ret, frame = capture.read()
     if not ret:
-        assert "Failed to capture an image !!!"
+        assert False, "Failed to capture an image !!!"
 
-    return ret, frame
     # Release the camera
     capture.release()
 
+    return ret, frame
+
 
 # action : manual = Please print any key to close the image.
-# action : other = Image will close after 10s.
-def showImage(action):
+# action : empty = Image will close after 10s.
+def showImage(action=None):
     ret, frame = captureImage()
 
     # Show the captured image
@@ -44,9 +56,67 @@ def showImage(action):
 
 
 def saveImage(fileName):
+    print("Ready to save the image ...")
     ret, frame = captureImage()
 
+    # The path of image to save
+    currentPath = os.getcwd()
+    testPatternPath = currentPath + "\\test_pattern\\"
+
     # Save the captured image
-    image_filename = fileName
-    cv2.imwrite(image_filename, frame)
-    print(f"Image captured and saved as {image_filename}")
+    imageFilename = testPatternPath + fileName
+    cv2.imwrite(imageFilename, frame)
+    print(f"Image captured and saved as {imageFilename}")
+
+    return imageFilename
+
+
+def removeImage(fileName):
+    print("Ready to remove the image ...")
+    currentPath = os.getcwd()
+    testPatternPath = currentPath + "\\test_pattern\\"
+    imageFilename = testPatternPath + fileName
+
+    try:
+        os.remove(imageFilename)
+        print(f"File {imageFilename} removed successfully")
+    except FileNotFoundError:
+        print(f"File {imageFilename} not found")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+def checkIfDisplayed(fileName):
+    # Get the capture from camera
+    imageFilename = saveImage("inTest.jpg")
+    time.sleep(3)
+
+    # Read the images
+    fromTestPattern = io.imread(fileName)
+    fromCameraCapture = io.imread(imageFilename)
+
+    # Convert the images to grayscale
+    fromTestPatternGray = color.rgb2gray(fromTestPattern)
+    fromCameraCaptureGray = color.rgb2gray(fromCameraCapture)
+
+    # Specify the data_range parameter (assuming 0 to 255 for typical images)
+    dataRange = fromTestPatternGray.max() - fromTestPatternGray.min()
+
+    # Calculate the SSIM score
+    similarityIndex, _ = ssim(fromTestPatternGray, fromCameraCaptureGray, full=True, data_range=dataRange)
+    print(f"SSIM: {similarityIndex}")
+    # Calculate the MSE score
+    mseScore = mse(fromTestPatternGray, fromCameraCaptureGray)
+    print(f"MSE: {mseScore}")
+
+    removeImage("inTest.jpg")
+
+    # # TODO: Waiting to discuss and test how much SSIM score needs to be greater than
+    # # A Higher score indicates higher similarity
+    # print(f"SSIM: {similarityIndex}")
+    # # A lower MSE indicates higher similarity
+    # print(f"MSE: {mseScore}")
+    # #
+    # print(f"NCC: {nccScore}")
+
+
--- OpenCVTest/main.py
+++ OpenCVTest/main.py
@@ -1,5 +1,14 @@
+import time
 import cvStep
 
+
+
 if __name__ == "__main__":
-    cvStep.setCamera(0)
-    cvStep.showImage("manual")
+
+   cvStep.setCamera(0)
+   imageFilename = cvStep.saveImage("test.jpg")
+
+   time.sleep(5)
+   cvStep.checkIfDisplayed(imageFilename)
+
+
